366a2aa85723b003459062e561ae83ec8d95c548
codingninjayj <jadhavpc0707@gmail.com>
2024-03-21 13:32:53 +0530

    Our First Program

--- code1.py
+++ code1.py
@@ -2,3 +2,14 @@ print("hello world")
 print(5)
 print("bye")
 print(17*13)
+
+
+#Our First Program
+#Today we will write our first ever python program from scratch. It will consist of a bunch of print statements. print can be used to print something on the console in python
+
+##Quick Quiz
+#Write a program to print a poem in Python. Choose the poem of your choice and publish your repl
+
+#print("---Your poem here---")
+
+#Please make sure you attempt this. Might be easy for some of you but please finish each and every task
