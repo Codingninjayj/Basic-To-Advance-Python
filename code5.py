b061200164bd1908f0dfffe466a1b9cb05964195
codingninjayj <jadhavpc0707@gmail.com>
2024-03-21 18:17:25 +0530

    Exercise 1- Create a Calculator

--- code5.py
+++ code5.py
@@ -1,16 +1,18 @@
 #Exercise 1- Create a Calculator
 
-n = 15
-m = 7
-ans1 = n+m
-print("Addition of",n,"and",m,"is", ans1)
-ans2 = n-m
-print("Subtraction of",n,"and",m,"is", ans2)
-ans3 = n*m
-print("Multiplication of",n,"and",m,"is", ans3)
-ans4 = n/m
-print("Division of",n,"and",m,"is", ans4)
-ans5 = n%m
-print("Modulus of",n,"and",m,"is", ans5)
-ans6 = n//m
-print("Floor Division of",n,"and",m,"is", ans6)
+a=10
+b=15
+ans1=a+b
+print("The addition of a + b is",ans1,)
+ans1=a-b
+print("The Subtraction of a + b is",ans1,)
+ans1=a*b
+print("The Multiplication of a + b is",ans1,)
+ans1=a/b
+print("The Division of a + b is",ans1,)
+ans1=a**b
+print("The Exponential of a + b is",ans1,)
+ans1=a%b
+print("The Modules of a + b is",ans1,)
+ans1=a//b
+print("The floor divisionof a + b is",ans1,)
