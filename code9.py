55fa9feb07fd0468babc86c29de50aa9d7437148
codingninjayj <jadhavpc0707@gmail.com>
2024-03-22 17:23:34 +0530

    Strings Slicing and Operations

---
+++ code9.py
@@ -0,0 +1,21 @@
+# String Slicing & Operations on String
+names="yash,jadhav"
+print(names[0:4])
+
+# Length of a String
+# We can find the length of a string using len() function.
+
+# Example:
+print(len(names))
+
+# Example:
+frute="harry"
+a=len(frute) 
+print(a)
+print(frute[0:4]) #including 0 but not 4
+print(frute[1:4]) #including 0 but not 4
+print(frute[:5])  #Slicing from Start
+print(frute[:-3]) #Slicing using negative index
+print(frute[-1:-3])
+print(frute[5:])  
+print(frute[-4:-2])
