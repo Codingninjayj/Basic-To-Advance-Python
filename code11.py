7adbd7cfd03816d0dbb3c90b5eaccc8218a02c42
codingninjayj <jadhavpc0707@gmail.com>
2024-03-23 19:07:00 +0530

    If-Else Condition

---
+++ code11.py
@@ -0,0 +1,22 @@
+# if-else Statements
+# Sometimes the programmer needs to check the evaluation of certain expression(s), whether the expression(s) evaluate to True or False. If the expression evaluates to False, then the program execution follows a different path than it would have if the expression had evaluated to True.
+
+a=int(input("Enter your age:"))
+print("Your age is:",a)
+
+#CONDITIONAL OPERATORS
+#>,<,>=,<=,==,!=
+print(a>18)
+print(a<18)
+print(a>=18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+
+if(a>18):
+    print("You can drive")
+else:
+    print("You can't drive")
+
+
+
