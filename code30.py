e75bd8fe882bd99b5afb6200d6f57fd480f5d81d
codingninjayj <jadhavpc0707@gmail.com>
2024-04-13 11:45:31 +0530

    Sets in Python

---
+++ code30.py
@@ -0,0 +1,23 @@
+# Python Sets
+# Sets are unordered collection of data items. They store multiple items in a single variable. Set items are separated by commas and enclosed within curly brackets {}. Sets are unchangeable, meaning you cannot change items of the set once created. Sets do not contain duplicate items.
+
+# Example:
+s={1,5,3,2,1,4,2,1}
+print(s)
+
+#Example
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+#Example:
+yash=set()
+print(type(yash))
+
+
+# Accessing set items:
+# Using a For loop
+# You can access items of set using a for loop.
+
+# Example:
+for value in info:
+    print(value)
